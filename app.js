8a777629d26330e06eb29a23d2b8fe34a637b715
aincatoni <ain.catoni@gmail.com>
2023-01-24 17:12:21 -0300

    carga nuevo js

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
 new Vue({
- el: "#productos-landings",
- data: function () {
-  return {
-   datasources: [],
-   products: [],
-  };
- },
- methods: {
-  consumingDataSources: function () {
-   axios
-    .get(
-     "https://stoplight.io/mocks/modyo/pcfactory-documentation/79796109/campanas/navidad"
-    )
-    .then((response) => {
-     this.datasources = response.data;
-    })
-    .catch((e) => {
-     console.log(e);
-    });
+  el: "#productos-landings",
+  data: function () {
+    return {
+      datasources: [],
+      products: [],
+    };
   },
-  consumingProducts: function () {
-   let arrDatas = this.datasources.map((data) => data.dataSource);
-   let arrproducts = [];
-   this.products = arrproducts;
-   for (datasource in arrDatas) {
-    axios
-     .get(
-      `https://stoplight.io/mocks/modyo/pcfactory-documentation/79796109/campanas/navidad/${datasource}`
-     )
-     .then((response) => {
-      arrproducts.push(response.data.content.items);
-     })
-     .catch();
-   }
+  methods: {
+    consumingDataSources: function () {
+      axios
+        .get(
+          "https://stoplight.io/mocks/modyo/pcfactory-documentation/79796109/campanas/navidad"
+        )
+        .then((response) => {
+          this.datasources = response.data;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+
+    consumingProducts: function () {
+      let arrDatas = this.datasources.map((data) => data.dataSource);
+      var arrProductos = [];
+      /* console.log(arrDatas); */
+      for (datasource in arrDatas) {
+        axios
+          .get(
+            `https://stoplight.io/mocks/modyo/pcfactory-documentation/79796109/campanas/navidad/${arrDatas[datasource]}`
+          )
+          .then((response) => {
+            arrProductos.push(...response.data.content.items);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
+      /* console.log(arrProductos); */
+      this.products = arrProductos;
+    },
   },
- },
- created: function () {
-  this.consumingDataSources();
- },
- beforeUpdate: function () {
-  this.consumingProducts();
- },
+  created: function () {
+    this.consumingDataSources();
+  },
+  updated: function () {
+    this.consumingProducts();
+  },
 });
